beeee4745267fc2c6badc888d9e259236ed44d40
Matt Archbell <72082201+mattarchbell@users.noreply.github.com>
2021-01-27 14:44:14 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,7 @@
-let map;
+let flemingmap;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  flemingmap = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 44.34, lng: -78.74 },
     zoom: 8,
   });
