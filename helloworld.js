f6971fb0d73cf699a07a63cdace2ff43ebca7554
Matt Archbell <72082201+mattarchbell@users.noreply.github.com>
2021-01-26 19:37:16 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 44.34, lng: -78.74 },
     zoom: 8,
   });
 }
